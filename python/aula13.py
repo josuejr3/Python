d5beaffdbcb6ae605edb2bed699eab6f1c2770f1
Josué Ferreira de Souza Júnior <100161905+josuejr3@users.noreply.github.com>
2025-01-01 09:31:42 -0300

    v8

--- python/aula13.py
+++ python/aula13.py
@@ -260,6 +260,8 @@ print(s1)
 
 # =================================== ISINSTANCE =====================================
 
+# serve para verificar se um objeto é de um determinado tipo ou
+
 lista = ['a', 1, 1.1, True, [0, 1, 2], (1, 2), {0, 1}, {"nome": "Luiz"}]
 
 for item in lista:
