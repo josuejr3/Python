19f755adbbf62aa8e6a61fcd5eec434a4d4d807b
Josue <geenujunior@gmail.com>
2025-03-29 20:08:23 -0300

    vault backup: 2025-03-29 20:08:22

--- python/aula23.py
+++ python/aula23.py
@@ -8,6 +8,7 @@ class MyList(Sequence):
     def __init__(self):
         self._data = {}
         self._index = 0
+        self._next_index = 0
 
     def append(self, value):
         self._data[self._index] = value
@@ -17,12 +18,23 @@ class MyList(Sequence):
         return self._index
 
     def __getitem__(self, index):
-        print()
         return self._data[index]
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._next_index >= self._index:
+            raise StopIteration
+        value = self._data[self._next_index]
+        self._next_index += 1
+        return value
 
 if __name__ == '__main__':
     lista = MyList()
     lista.append('maria')
     lista.append('joao')
-    print(lista[0])
+    # print(lista[0])
+
+    for item in lista:
+        print(item)
