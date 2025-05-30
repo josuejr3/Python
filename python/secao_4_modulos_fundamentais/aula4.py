678aef8cc9b3242dcdd1eb587ab3ec75de34b1eb
Josue <geenujunior@gmail.com>
2025-05-30 18:40:08 -0300

    vault backup: 2025-05-30 18:40:08

--- python/secao_4_modulos_fundamentais/aula4.py
+++ python/secao_4_modulos_fundamentais/aula4.py
@@ -3,3 +3,5 @@ from datetime import datetime
 fmt = "%d/%m/%Y %H:%M:%S"
 data_inicio = datetime.strptime("20/04/1987 09:30:30", fmt)
 data_fim = datetime.strptime("12/12/2022 08:20:20", fmt)
+
+print(data_fim > data_inicio)
--- secao_4_modulos_fundamentais/Aula 4.md
+++ secao_4_modulos_fundamentais/Aula 4.md
@@ -1,3 +1,10 @@
 
 #### <span style="color:rgb(4, 255, 0)">Cálculo de datas</span> 
 
+O cálculo de datas serve para compararmos por exemplo se uma data é menor, maior ou igual outra.
+
+> Exemplo
+
+```Python
+
+```
