c4bb933562a557130f3b0fa0f45e6ac07e648fc9
Josue <geenujunior@gmail.com>
2025-05-30 18:42:11 -0300

    vault backup: 2025-05-30 18:42:11

--- python/secao_4_modulos_fundamentais/aula4.py
+++ python/secao_4_modulos_fundamentais/aula4.py
@@ -4,4 +4,4 @@ fmt = "%d/%m/%Y %H:%M:%S"
 data_inicio = datetime.strptime("20/04/1987 09:30:30", fmt)
 data_fim = datetime.strptime("12/12/2022 08:20:20", fmt)
 
-print(data_fim > data_inicio)
+print(data_fim - data_inicio)
