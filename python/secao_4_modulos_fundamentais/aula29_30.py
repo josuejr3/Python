5e90e51daa62f6bef7aacfbceb004e96a7cbf539
Josue <geenujunior@gmail.com>
2025-06-06 18:22:39 -0300

    vault backup: 2025-06-06 18:22:38

--- python/secao_4_modulos_fundamentais/aula29_30.py
+++ python/secao_4_modulos_fundamentais/aula29_30.py
@@ -3,6 +3,9 @@
 
 import os
 from dotenv import load_dotenv # type: ignore
+from pathlib import Path
+
+from aula18 import print_iter
 
 load_dotenv(verbose=True)
 
@@ -12,9 +15,60 @@ destinatario = remetente
 
 # Configurações do servidor SMTP
 
-smtp_server = "smtp.google.com"
+# smtp da google
+smtp_server = "smtp.gmail.com"
 smtp_port = 587
 smtp_user = os.getenv("FROM_EMAIL", "")
 smtp_passw = os.getenv('EMAIL_PASSWORD')
 
- 
+# Configuração - Mensagem de Texto
+
+# Gerando caminho do arquivo
+CAMINHO_MSG = Path(__file__).parent / "aula29_30_email.html"
+
+with open(CAMINHO_MSG, 'r', encoding="utf-8") as f:
+    texto_arquivo = f.read()
+
+# Transformar mensagem em MIMEMultipart
+# MIMEMultipart basicamente eu passo informações de from, to e subject
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+mime_multipart = MIMEMultipart()
+mime_multipart["from"] = remetente
+mime_multipart["to"] = destinatario
+mime_multipart["subject"] = "Este é o assunto"
+
+# Criando o corpo do texto
+corpo_email = MIMEText(texto_arquivo, "html", "utf-8")
+# Anexa o corpo do texto
+mime_multipart.attach(corpo_email)
+
+# Abrindo o servidor smtp
+import smtplib
+
+# Enviando e-mail
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.ehlo()
+    server.starttls()
+    server.login(smtp_user, smtp_passw)
+    server.send_message(mime_multipart)
+    print('E-mail enviado com sucesso')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
