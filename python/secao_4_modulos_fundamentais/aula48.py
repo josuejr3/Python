1bead986ea0beeca173e2013ecec8627a94f2cff
Josue <geenujunior@gmail.com>
2025-06-08 15:33:13 -0300

    vault backup: 2025-06-08 15:33:13

--- python/secao_4_modulos_fundamentais/aula48.py
+++ python/secao_4_modulos_fundamentais/aula48.py
@@ -1,1 +1,52 @@
-from thread 
+from threading import Thread
+from time import sleep
+
+# Criando threads
+
+class MyThread(Thread):
+    def __init__(self, texto, tempo):
+        self.texto = texto
+        self.tempo = tempo
+
+        super().__init__()
+
+    def run(self):
+        sleep(self.tempo)
+        print(f"{self.texto} foi finalizada!")
+
+# Criando minha thread
+t1 = MyThread('Thread 1', 5)
+# Iniciando a execução da thread
+t1.start()
+
+t2 = MyThread('Thread 2', 2)
+t2.start()
+
+t3 = MyThread('Thread 3', 10)
+t3.start()
+
+for i in range(10):
+    print(i)
+    sleep(1)
+
+######## Outra forma - Aula 49
+
+def vai_demorar(tempo: int, texto: str) -> None:
+    sleep(tempo)
+    print(texto)
+
+thread_1 = Thread(target=vai_demorar, args=(10, "Terminou t1"))
+thread_1.start()
+
+# executando a main thread
+
+# while thread_1.is_alive():
+#     print('Esperando a thread.')
+#     sleep(2)
+
+t1.join()
+
+# Posso fazer a thread principal esperar a thread "secundaria"
+# finalizar para que ela continue, basta fazer o seguinte código
+
+print('Acabou')
--- secao_4_modulos_fundamentais/Aula 48.md
+++ secao_4_modulos_fundamentais/Aula 48.md
@@ -13,3 +13,49 @@ for i in range(10):
 	sleep(.5)
 print("World")
 ```
+
+```Python
+from threading import Thread  
+from time import sleep  
+  
+# Criando threads  
+  
+class MyThread(Thread):  
+    def __init__(self, texto, tempo):  
+        self.texto = texto  
+        self.tempo = tempo  
+  
+        super().__init__()  
+  
+    def run(self):  
+        sleep(self.tempo)  
+        print(f"{self.texto} foi finalizada!")  
+  
+# Criando minha thread  
+t1 = MyThread('Thread 1', 5)  
+# Iniciando a execução da thread  
+t1.start()  
+  
+for i in range(10):  
+    print(i)  
+    sleep(1)
+```
+
+No código acima, primeiro nós criamos uma classe de threads que herda a classe de threads principal e sobreescrevemos o método run. 
+
+Após isso, instanciamos uma thread 1 e a iniciamos. Após a finalização dessa thread ela exibe uma mensagem informando que foi finalizada. Com isso conseguimos perceber que mesmo que um for esteja rodando, a thread também estará rodando ao mesmo tempo.
+
+```cmd
+0
+1
+2
+3
+4
+Thread 1 foi finalizada!
+5
+6
+7
+8
+9
+```
+
