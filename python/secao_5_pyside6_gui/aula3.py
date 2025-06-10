d59cccea2b434286f3c28316008c852229379046
Josue <geenujunior@gmail.com>
2025-06-10 18:23:26 -0300

    vault backup: 2025-06-10 18:23:26

---
+++ python/secao_5_pyside6_gui/aula3.py
@@ -0,0 +1,26 @@
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QGridLayout
+
+app = QApplication()
+
+botao = QPushButton("Botão 1")
+botao.setStyleSheet("font-size: 25px; color: #0000FF; font-weight: bold;")
+
+botao2 = QPushButton("Botão 2")
+botao2.setStyleSheet("font-size: 25px; color: #0000FF; font-weight: bold;")
+
+botao3 = QPushButton("Botão 3")
+botao3.setStyleSheet("font-size: 25px; color: #0000FF; font-weight: bold;")
+
+# Widget generico
+central_widget = QWidget()
+# Cria um layout vertical
+layout = QGridLayout()
+central_widget.setLayout(layout)
+# Adicionando o botao ao layout
+layout.addWidget(botao, 1, 1, 1, 1)
+layout.addWidget(botao2, 1, 2, 1, 1)
+layout.addWidget(botao3, 1, 1, 3, 1)
+
+# Mostre na janela
+central_widget.show()
+app.exec()
---
+++ secao_5_pyside6_gui/Aula 3.md
@@ -0,0 +1,7 @@
+
+#### <span style="color:rgb(4, 255, 0)">Resolvendo o problema de uma janela por widgets</span> 
+
+Para resolver o problema de uma janela por widget vamos usar um widget especial, chamado de central widget.
+
+Basicamente, quando temos uma janela, teremos um widget central que vai ser o único widget presente. A partir desse widget, aplicamos um layout e o layout é que vai "absorver" os widgets restantes.
+
