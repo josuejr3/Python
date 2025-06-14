321777f542f33bf306f8acdc62b1f2a127935b58
Josue <geenujunior@gmail.com>
2025-06-13 23:31:17 -0300

    vault backup: 2025-06-13 23:31:17

--- python/calculator/MainWindow.py
+++ python/calculator/MainWindow.py
@@ -1,18 +1,22 @@
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
+from constants import *
 
 class MainWindow(QMainWindow):
     # O argumento parent é uma "janela pai/mãe"
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
 
+        # Configuração Layout básico
+
         # Definindo o Widget central
         self.central_widget = QWidget()
 
         # Definindo o layout do Widget central
-        self.layout_to_cwidget = QVBoxLayout()
+        self.layout_to_central_widget = QVBoxLayout()
 
         # Aplicando o layout no Widget Central
-        self.central_widget.setLayout(self.layout_to_cwidget)
+        self.central_widget.setLayout(self.layout_to_central_widget)
 
         # Aplicando o Widget Central a minha Window
         self.setCentralWidget(self.central_widget)
@@ -20,12 +24,18 @@ class MainWindow(QMainWindow):
         # Alterando o nome da janela principal
         self.setWindowTitle("Calculator")
 
+        # Criando o ícone e alterando na janela
+        self.icon = QIcon(str(WINDOW_ICON_PATH))
+        self.setWindowIcon(self.icon)
 
-    def adjustFixedSize(self):
-        
+    def addWidgetToVLayout(self, widget: QWidget) -> None:
+        self.layout_to_central_widget.addWidget(widget)
+        # self.adjustFixedSize()
+
+    def adjustFixedSize(self) -> None:
         # Última coisa a ser feita
-        # self.adjustSize()
-        # self.setFixedSize(self.width(), self.height())
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
 
 
 
--- python/calculator/main.py
+++ python/calculator/main.py
@@ -1,14 +1,20 @@
 import sys
 
-from PySide6.QtCore import QSize
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
 
 from MainWindow import MainWindow
+from calculator.constants import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    label1 = QLabel('1')
+    label2 = QLabel('2')
+    window.addWidgetToVLayout(label1)
+    window.addWidgetToVLayout(label2)
 
     window.show()
+
     app.exec()
